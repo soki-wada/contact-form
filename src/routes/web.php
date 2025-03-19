9f69bcc6960db6b7d6f7d194a9b941a1f70fc519
soki_wada <swmk1sb@gmail.com>
2025-03-20 00:38:26 +0900

    1-5 complete

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 // });
 Route::get('/', [ContactController::class, 'index']);
 Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
